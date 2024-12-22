d6e1edcc6d9eb88ddc69f64c171c9f61dd907906
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2024-12-22 15:01:13 -0800

    Coming Soon Message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import BlackLogo from './images/BLogo.png';
 
 function App() {
+  const [dots, setDots] = useState('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDots((prev) => (prev.length < 3 ? prev + '.' : ''));
+    }, 500); 
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <div className="App">
-      <h3>Alberto's Portfolio</h3>
-      <p>Coming Soon...</p>
+      <img src={BlackLogo} alt="Logo" className="Logo"></img>
+      <h3>Alberto Rubalcava's Portfolio</h3>
+      <p>Coming Soon{dots}</p>
     </div>
   );
 }
