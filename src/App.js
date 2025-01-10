6e84384d8d3879d964a94e081f41c21e167b6b7b
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2025-01-10 01:28:13 -0800

    navbar and introduction section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import BlackLogo from './images/BLogo.png';
+import Navbar from './Navbar';
+import '@fortawesome/fontawesome-free/css/all.css';
+import Introduction from './Introduction';
+
 
 function App() {
-  const [dots, setDots] = useState('');
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDots((prev) => (prev.length < 3 ? prev + '.' : ''));
-    }, 500); 
-
-    return () => clearInterval(interval); 
-  }, []);
 
   return (
     <div className="App">
-      <img src={BlackLogo} alt="Logo" className="Logo"></img>
-      <h3>Alberto Rubalcava's Portfolio</h3>
-      <p>Coming Soon{dots}</p>
+          <Navbar />
+          <Introduction />
     </div>
   );
 }
---
+++ src/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+import BlackLogo from './images/BLogo.png';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div><img className="logo" src={BlackLogo}></img></div>
+      <div className="nav-links">
+        <a href="#experience">experience</a>
+        <a href="#projects">projects</a>
+        <a href="#contact">contact</a>
+        <a href="#resume">resume</a>
+        <a href="https://github.com/AlbertoRubalcava" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-github"></i>
+        </a>
+        <a href="https://linkedin.com/in/alberto-rubalcava" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
