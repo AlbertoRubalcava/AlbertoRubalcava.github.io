625db617200bee40343b79129956759d7f594bae
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2025-01-12 00:42:07 -0800

    resume added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Navbar.css';
 import BlackLogo from './images/BLogo.png';
+import resume from './images/Alberto Rubalcava Resume Portfolio.pdf'
 
 const Navbar = () => {
   return (
@@ -10,7 +11,7 @@ const Navbar = () => {
         <a href="#experience">experience</a>
         <a href="#projects">projects</a>
         <a href="#contact">contact</a>
-        <a href="#resume">resume</a>
+        <a href={resume} target="_blank" rel="noopener noreferrer">resume</a>
         <a href="https://github.com/AlbertoRubalcava" target="_blank" rel="noopener noreferrer">
           <i className="fab fa-github"></i>
         </a>
