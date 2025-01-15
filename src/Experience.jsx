7ec6de9e5b3690efad74da873effee954203eb6d
alberto rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2025-01-14 17:18:58 -0800

    timeline updated

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,58 +1,50 @@
-import React from 'react';
-import './Experience.css';
-import CSUN from './images/CSUN.png';
-import HHC from './images/HHC.jpg';
+// Experience.js
+import React from "react";
+import "./Experience.css";
+import CSUN from "./images/CSUN.png";
+
+const events = [
+  {
+    date: "June 2024 - Present",
+    title: "Web Developer Student Assistant",
+    company: "CSUN IT",
+    description:
+      "Develop an AI-focused course recommendation planner in Python utilizing 20 years of CSUN course data, enhancing student academic planning by recommending classes based on completed coursework and academic goals. Work with other student assistants and professionals to ensure the model has a 95% accuracy benchmark before deploying the tool, ensuring reliability to students and advisors.",
+    image: CSUN,
+  },
+  {
+    date: "June 2023 - June 2024",
+    title: "Admin & Finance Student Assistant",
+    company: "CSUN IT",
+    description:
+      "Assisted and supported IT directors, staff, and faculty with daily tasks such as managing 150 IT student assistants' payroll. Gained exposure to the daily operations of numerous departments throughout the IT Department such as Information Security, Database, App Development, and Web Development.",
+    image: CSUN,
+  },
+];
 
 const Experience = () => {
-  const experiences = [
-    {
-      year: "2024 - Present",
-      title: "Web Developer SA",
-      company: "CSUN IT",
-      description: "• Develop an AI focused course recommendation planner in Python utilizing 20 years of CSUN course data, enhancing student academic planning by recommending classes based on completed coursework and academic goals.<br />• Work with other student assistants and professionals to ensure the model has a 95% accuracy benchmark before deploying the tool, ensuring reliability to students and advisors.",
-      image: CSUN
-    },
-    {
-      year: "2023 - 2024",
-      title: "Admin & Finance SA",
-      company: "CSUN IT",
-      description: "• Assisted and supported IT directors, staff, and faculty with daily tasks such as managing 150 IT student assistant's payroll. <br /> • Gained exposure to the daily operations of numerous departments throughout the IT Department such as Information Security, Database, App Development, and Web Development. ",
-      image: CSUN
-    },
-    {
-      year: "2022-2023",
-      title: "Cashier/Cook",
-      company: "Hawaiian Hot Chicken",
-      description: "• Maintain excellent customer service during fast-paced rushes. <br/>• Prepare and fulfill food orders quickly and accurately. <br />• Take the initiative to find extra tasks when scheduled duties are completed.",
-      image: HHC
-    },
-  ];
-
   return (
-    <div className="work-experience" id="experience">
-      <h2 className='title' >Work Experience</h2>
-      <div className="timeline-container">
-        <div className="timeline">
-          {experiences.map((exp, index) => (
-            <div className="timeline-item" key={index}>
-              <div className={`timeline-content ${index % 2 === 0 ? "above" : "below"}`}>
-                <h3>{exp.title}</h3>
-                <div className="header-container">
-                  <h4>{exp.company}</h4>
-                  <p className="year">{exp.year}</p>
-                </div>
-                <p dangerouslySetInnerHTML={{ __html: exp.description }} />
-              </div>
-              <div className="timeline-circle">
-                <img src={exp.image} alt={exp.title} className="circle-image" />
-              </div>
-            </div>
-          ))}
-        </div>
-        <div className="central-line"></div>
-      </div>
+    <div className='margining'id="experience">
+      <nav class="tmln tmln--box tmln--hr" >
+        <h2 class="tmln__header" >Work Experience</h2>
+          <ul class="tmln__list">
+            <li class="tmln__item tmln__item--bg-lght">
+              <span data-title>June 2024 - Present</span>
+              <h3 class="tmln__item-headline">Web Development - Student Assistant</h3>
+              <h4 class="tmln__item-headline">CSUN IT</h4>
+              <p>• Develop an AI-focused course recommendation planner in Python utilizing 20 years of CSUN course data, enhancing student academic planning by recommending classes based on completed coursework and academic goals.<br/>• Work with other student assistants and professionals to ensure the model has a 95% accuracy benchmark before deploying the tool, ensuring reliability to students and advisors.</p>
+            </li>
+            <li class="tmln__item tmln__item--bg-lght">
+              <span data-title>June 2023 - June 2024</span>
+              <h3 class="tmln__item-headline">Admin & Finance - Student Assistant</h3>
+              <h4 class="tmln__item-headline">CSUN IT</h4>
+              <p>• Assisted and supported IT directors, staff, and faculty with daily tasks such as managing 150 IT student assistants' payroll.<br/>• Gained exposure to the daily operations of numerous departments throughout the IT Department such as Information Security, Database, App Development, and Web Development.</p>
+            </li>
+          </ul>
+      </nav>
     </div>
   );
 };
 
 export default Experience;
+
