08069938fbcec50ca8f9ae2715586cadea75ea82
Alberto Rubalcava <137430320+AlbertoRubalcava@users.noreply.github.com>
2026-01-14 14:11:28 -0800

    added senior design project

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -4,11 +4,19 @@ import SDC from './images/SDC.png';
 import CGRP from './images/CGRP.png';
 import JAM from './images/JAM.png';
 import SS12 from './images/SS12.png';
+import QL from './images/QuickLock.png';
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Projects = () => {
   const projects = [
+    {
+      title: "Senior Design - QuickLock",
+      description:
+        "QuickLock is a smart lock system that lets users manage and control access to an area through NFC cards and a mobile app. It eliminates the use of traditional keys and improves upon their vulnerabilities by giving users complete visibility and control over who can access a space and at what time. As the Student Embedded Systems Engineer for this project, I was responsible for designing, implementing, and maintaining all firmware components, including NFC communication, access control logic, and hardware-level integration.",
+      image: QL,
+      link: 'https://github.com/vicat0ria/chipi-chipi/',
+    },
     {
       title: "SS12 Code for a Cause 2025",
       description:
@@ -31,7 +39,7 @@ const Projects = () => {
       link: 'https://github.com/AlbertoRubalcava/SDC2024',
     },
     {
-      title: "AI Jam 2024",
+      title: "CourseCompass - AI Jam 2024",
       description:
         "Secured 3rd place in a university-wide AI competition by developing a prototype of a personalized AI-driven course planner to optimize student schedules. Designed and developed a website prototype, and contributed to the research and implementation.",
       image: JAM,
